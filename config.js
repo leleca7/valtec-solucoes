b587b0af4168bfc830de856b487b1797cc441574
leleca7 <wanelle52@gmail.com>
2026-08-17 17:54:43 -0300

    Libera login para administradores validados no Supabase

--- config.js
+++ config.js
@@ -5,7 +5,9 @@ window.VALTEC_CONFIG = {
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_AByBURj4aHtdshWOqzIQCg_qpsHqDxz",
   WHATSAPP_NUMBER: "5571981954452",
   PHONE_NUMBER: "",
-  ADMIN_EMAIL_SHA256: "6982fe850f748a2451adca265f14939a0709e330c99c3dcdfaa6e807400b7187"
+  // A autorização real dos administradores é validada no banco pelo perfil do usuário.
+  // Mantemos vazio para permitir que os e-mails autorizados recebam o link de acesso.
+  ADMIN_EMAIL_SHA256: ""
 };
 
 // Compatibilidade da identidade visual: qualquer componente antigo que ainda crie
