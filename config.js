bff84805f0170767fae19efb8919c07a4e0e2c0a
leleca7 <wanelle52@gmail.com>
2026-08-17 14:00:19 -0300

    configura WhatsApp e e-mail administrativo

--- config.js
+++ config.js
@@ -3,6 +3,7 @@
 window.VALTEC_CONFIG = {
   SUPABASE_URL: "https://msgwcwpvjgjtqhktuust.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_AByBURj4aHtdshWOqzIQCg_qpsHqDxz",
-  WHATSAPP_NUMBER: "",
-  PHONE_NUMBER: ""
+  WHATSAPP_NUMBER: "5571981954452",
+  PHONE_NUMBER: "",
+  ADMIN_EMAIL: "vl.valdemir7@gmail.com"
 };
